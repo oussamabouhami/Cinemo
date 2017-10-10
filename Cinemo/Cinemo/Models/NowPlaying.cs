e2d0d4aa23ba737eccc5e1543f17ba14941c85cd
oussama bouhami <oussama_bouhami@outlook.com>
2017-10-10 10:49:59 +0100

    Maj 2

--- Cinemo/Cinemo/Models/NowPlaying.cs
+++ Cinemo/Cinemo/Models/NowPlaying.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cinemo
+namespace Cinemo.Models
 {
     class NowPlaying
     {
---
+++ Cinemo/Cinemo/Models/UpComing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemo.Models
+{
+    public class UpComing
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string Cast { get; set; }
+        public string Description { get; set; }
+        public string MovieLanguage { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string RatedPGI { get; set; }
+        public string MovieType { get; set; }
+        public object TrailorLink { get; set; }
+        public string Logo { get; set; }
+        public object LogoFile { get; set; }
+
+
+    }
+     
+    public class Rootobject
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string Cast { get; set; }
+        public string Description { get; set; }
+        public string MovieLanguage { get; set; }
+        public string Duration { get; set; }
+        public DateTime ShowTime { get; set; }
+        public string TicketPrice { get; set; }
+        public string MoviePrint { get; set; }
+        public string RatedPGI { get; set; }
+        public string MovieType { get; set; }
+        public string TrailorLink { get; set; }
+        public string Logo { get; set; }
+        public object LogoFile { get; set; }
+    }
+
+   
+}
