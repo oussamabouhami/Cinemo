663734f612dafae016386b3d74ccc8b6b6b8a011
oussama bouhami <oussama_bouhami@outlook.com>
2017-10-10 09:39:29 +0100

    Ajoutez des fichiers projet.

---
+++ Cinemo/Cinemo/NowPlaying.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemo
+{
+    class NowPlaying
+    {
+
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string Cast { get; set; }
+        public string Description { get; set; }
+        public string MovieLanguage { get; set; }
+        public string Duration { get; set; }
+        public DateTime ShowTime { get; set; }
+        public string TicketPrice { get; set; }
+        public string MoviePrint { get; set; }
+        public string RatedPGI { get; set; }
+        public string MovieType { get; set; }
+        public string TrailorLink { get; set; }
+        public string Logo { get; set; }
+        public string PhotoFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Logo))
+                {
+                    return string.Empty;
+                }
+                return string.Format("http://cinemo.azurewebsites.net/{0}", Logo.Substring(1));
+            }
+        }
+        public object LogoFile { get; set; }
+    }
+}
+
---
+++ Cinemo/Cinemo/UpComing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinemo
+{
+    class UpComing
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string Cast { get; set; }
+        public string Description { get; set; }
+        public string MovieLanguage { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string RatedPGI { get; set; }
+        public string MovieType { get; set; }
+        public object TrailorLink { get; set; }
+        public string Logo { get; set; }
+        public object LogoFile { get; set; }
+    }
+}
